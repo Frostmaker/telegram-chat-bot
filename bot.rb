b6ff91b5a42f0a2f2df823159ee98d5df6729f11
Falin <falin.original@gmail.com>
2022-04-20 14:54:40 +0300

    Add record in json file

--- bot.rb
+++ bot.rb
@@ -1,12 +1,13 @@
-require 'rubygems'
-require 'bundler/setup'
+require 'json'
 require 'telegram/bot'
 require 'colorize'
 require './constants'
 
 Telegram::Bot::Client.run(TOKEN) do |bot|
   puts "--- START ---".colorize(:green)
-
+  
+  json_file = JSON.parse(File.read("users_list.json"))
+  
   bot.listen do |message|
     case message.text
     when '/start'
@@ -14,10 +15,22 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
     else 
+      unless json_file.include?(message.from.id.to_s)
+        json_file[message.from.id] = [message.from.first_name, message.from.last_name].join(" ").rstrip
+      end
+
+      output = File.open("users_list.json", "w+")
+      output.write(JSON.generate(json_file))
+      output.close
+
       print "[".colorize(:red) + "#{(Time.at(message.date)).to_s[11..15].colorize(:red)}" + "]".colorize(:red) + " #{message.from.first_name.colorize(:blue)}#{message.from.last_name.nil? ? '' : ' ' + message.from.last_name.colorize(:blue)}: #{message}\n"
       print ">> ".colorize(:green)
       answer = gets.chomp
       bot.api.send_message(chat_id: message.chat.id, text: answer) if !answer.empty?
+    
     end
+  
+
+  
   end
 end
