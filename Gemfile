375eca3c859b5e127ce79f3513553676e682485b
Falin <falin.original@gmail.com>
2022-04-20 16:15:24 +0300

    Change user id to chat id

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-gem 'telegram-bot-ruby', '0.19.2'
 gem 'colorize', '~> 0.8.1'
+gem 'telegram-bot-ruby', '0.19.2'
 gem 'time'
--- bot.rb
+++ bot.rb
@@ -3,34 +3,32 @@ require 'telegram/bot'
 require 'colorize'
 require './constants'
 
+threads = []
+
 Telegram::Bot::Client.run(TOKEN) do |bot|
-  puts "--- START ---".colorize(:green)
-  
-  json_file = JSON.parse(File.read("users_list.json"))
-  
-  bot.listen do |message|
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-    when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-    else 
-      unless json_file.include?(message.from.id.to_s)
-        json_file[message.from.id] = [message.from.first_name, message.from.last_name].join(" ").rstrip
+  puts '--- START ---'.colorize(:green)
+
+  json_file = JSON.parse(File.read('users_list.json'))
+    bot.listen do |message|
+      unless json_file.has_value?(message.chat.id)
+        json_file[[message.from.first_name, message.from.last_name].join(' ').rstrip] = message.chat.id 
       end
 
-      output = File.open("users_list.json", "w+")
+      output = File.open('users_list.json', 'w+')
       output.write(JSON.generate(json_file))
       output.close
 
-      print "[".colorize(:red) + "#{(Time.at(message.date)).to_s[11..15].colorize(:red)}" + "]".colorize(:red) + " #{message.from.first_name.colorize(:blue)}#{message.from.last_name.nil? ? '' : ' ' + message.from.last_name.colorize(:blue)}: #{message}\n"
-      print ">> ".colorize(:green)
-      answer = gets.chomp
-      bot.api.send_message(chat_id: message.chat.id, text: answer) if !answer.empty?
+      print '['.colorize(:red) + Time.at(message.date).to_s[11..15].colorize(:red).to_s + ']'.colorize(:red) + " #{message.from.first_name.colorize(:blue)}#{message.from.last_name.nil? ? '' : ' ' + message.from.last_name.colorize(:blue)}: #{message}\n"
     
+      print '>> '.colorize(:green)
+      answer = gets.chomp
+      bot.api.send_message(chat_id: message.chat.id, text: answer) unless answer.empty?
     end
-  
 
   
-  end
+
+
+  # threads << reciever
+  # threads << sender
+  # threads.each(&:join)
 end
