41ea59d379addce5ebac8f6fe73fc842ef6ec966
Falin <falin.original@gmail.com>
2022-04-21 19:28:35 +0300

    Print list of known users

--- sender.rb
+++ sender.rb
@@ -1,9 +1,18 @@
 require 'colorize'
 require 'socket'
+require 'json'
 
 server = TCPServer.new 2000 # Server bound to port 2000
 text = ''
 puts '--- START SENDER ---'.colorize(:green)
+
+json_file = File.read("users_list.json")
+json_file = JSON.parse json_file
+
+print "CHATS: \n".colorize(:blue)
+json_file.each {|key, val| puts key.colorize(:yellow)}
+
+user = nil
 
 loop do
   client = server.accept    # Wait for a client to connect
